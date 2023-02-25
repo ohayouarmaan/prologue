3cb3a98549633a80e7d0ab53d8a3cbb27f4437c0
Armaan Gupta <ohayouarmaan@gmail.com>
2023-02-25 16:25:48 +0530

    Added partial generate function in the timeline

--- Editor.py
+++ Editor.py
@@ -1,4 +1,3 @@
-# Import statements
 import ffmpeg
 import os
 import toml
@@ -22,8 +21,10 @@ class Media:
         self.inp = filter(self, **args)
 
 class Timeline:
-    def __init__(self, medias: list[Media]):
+    def __init__(self, medias: list[Media], default_dimensions=(1920, 1080)):
         self.medias = medias
+        self.default_dimensions = default_dimensions
+        self.inputs = []
         
     def add(self, media: Media) -> bool:
         if media:
@@ -31,6 +32,24 @@ class Timeline:
             return True
         else:
             return False
+    
+    def generate(self):
+        c = []
+        for m in self.medias:
+            s = float(ffmpeg.probe(m.path)['streams'][0]['duration'])
+            c.append(s)
+        
+        null_stream_length = sum(c)
+        v_null_stream = ffmpeg.input(f"nullsrc=size={self.default_dimensions[0]}x{self.default_dimensions[1]}")
+        a_null_stream = ffmpeg.input(f"anullsrc", f="lavfi", t=f"{null_stream_length / 1000}")
+        self.inputs.append({
+            "start": 0,
+            "end": null_stream_length,
+            "vstream": v_null_stream,
+            "astream": a_null_stream
+        })
+        
+
 
 class Editor:
     def __init__(self):
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from functions import cut
 
 e = Editor()
 m = Media("./testfiles/1.mp4")
-m.apply(cut.cut, {
-    "_from": 0,
-    "_to": 4
-})
+m2 = Media("./testfiles/1.mp4")
+
+e.default_timeline.add(m)
+e.default_timeline.add(m2)
+print(e.default_timeline.generate())
