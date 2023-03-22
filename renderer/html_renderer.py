da65e10bdf53e7125421e3fbce732f50e4c2346e
Armaan Gupta <ohayouarmaan@gmail.com>
2023-03-22 12:14:25 +0530

    Added selenium tab opening with random colors

--- renderer/html_renderer.py
+++ renderer/html_renderer.py
@@ -1,12 +1,16 @@
 import os
 import sys
 import selenium
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 class Renderer:
     def __init__(self, exec_path="./chromedriver.exe"):
-        self.driver_options = selenium.webdriver.chrome.options.Options()
+        self.driver_options = webdriver.chrome.options.Options()
         self.driver_options.add_experimental_option("detach", True)
-        self.driver = selenium.webdriver.Chrome(executable_path=os.path.abspath(exec_path), chrome_options=self.driver_options)
+        self.driver_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        self.driver = webdriver.Chrome(executable_path=os.path.abspath(exec_path), chrome_options=self.driver_options)
         self.inputs = {}
 
     def add_html(self, src: str):
@@ -15,6 +19,7 @@ class Renderer:
             _type = "url"
         elif os.path.isfile(src):
             _type = "file"
+            src = os.path.abspath(src)
 
         if len(list(self.inputs.keys())) == 0:
             self.inputs[0] = {
@@ -22,7 +27,7 @@ class Renderer:
                 "type": _type
             }
         else:
-            self.inputs[len(list(self.inputs.keys())) - 1] = {
+            self.inputs[len(list(self.inputs.keys()))] = {
                 "src": src,
                 "type": _type
             }
@@ -43,7 +48,32 @@ class Renderer:
                 "src": markup,
                 "type": _type
             }
+
+    def __open_new_tab(self, _id, color):
+        body = self.driver.find_element(By.TAG_NAME, "body")
+        self.driver.execute_script('''
+            window.open('', '_blank');
+        ''')
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        self.driver.get('file://' + os.path.abspath(self.inputs[_id]['src']))
+        self.driver.execute_script(f'''
+            window.document.getElementsByTagName('body')[0].style.backgroundColor = '{color}';
+        ''')
     
     def render(self):
-        pass
+        for _id in self.inputs:
+            color = f"rgb({random.randint(1, 255)}, {random.randint(1, 255)}, {random.randint(1, 255)})"
+            print(_id)
+            self.__open_new_tab(_id, color)
+        
+        self.driver.switch_to.window(self.driver.window_handles[0])
+        self.driver.close()
 
+
+
+
+if __name__ == "__main__":
+    r = Renderer()
+    r.add_html("./test.html")
+    r.add_html("./test copy.html")
+    r.render()
